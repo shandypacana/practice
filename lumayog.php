e47a36adceb58574a9dfd66b4e687ea01f37e8bb
unknown <klumayog@gmail.com>
2024-12-10 21:39:35 +0800

    wait lang

--- lumayog.php
+++ lumayog.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kenji Lumayog</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Kenji Lumayog</h1>
-        <p>Student of Phinma COC Bs IT 2nd year</p>
-        <p>My hobby is playing games, watching anime, coding, editing, and sleeping hehehehe</p>
-    </div>
-</body>
-</html>
+<img src="horn.jpg">
+
+<h1><strong>Kenji Lumayog<strong></h1>
+    <p>Student of Phinma COC Bs IT 2nd year </p>
+    <p>My hobby is playing games,watching anime,coding,editing,editing and sleeping hehehehe</p>
